56b0315efdb94a910516ca0299dd8b84d5b690b0
Mateus Brandão <mateusfleria@gmail.com>
2023-04-15 12:17:37 -0300

    Componentizando card de produtos

--- index.php
+++ index.php
@@ -40,30 +40,7 @@
     </nav>
 
     <section class="mt-5">
-      <div class="card-product card" style="width: 18rem;">
-        <div class="card-body">
-          <img
-            src="https://cdn.awsli.com.br/1000x1000/608/608801/produto/133246931/44b83527a3.jpg"
-            alt="Imagem da camisa software developer preta"
-            class="card-img-top rounded shadow"
-          />
-          <div class="d-flex flex-column mt-2">
-            <h5 class="card-title fs-6 mb-0">Camisa Software Developer preta</h5>
-            <span class="fs-4 fw-semibold">R$ 179,90</span>
-  
-            <button type="button" class="btn btn-primary mt-3 d-flex align-items-center p-0 text-sm overflow-hidden">
-              <div
-                class="d-flex align-items-center"
-                style="gap: .3rem; background: var(--bs-btn-active-bg); padding: .375rem .5rem;"
-              >
-                <?php include 'src/components/icons/add-card.php'; ?>
-                0
-              </div>
-              <span class="text-uppercase text-center px-2 text-xs fw-semibold" style="flex: 1;">Adicionar ao carrinho</span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <?php include 'src/components/product-item.php'; ?>
     </section>
   </main>
   <script
