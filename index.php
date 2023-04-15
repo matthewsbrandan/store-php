073d847b4497d7982c4b7ef43b7d4b8037ae62e0
Mateus Brandão <mateusfleria@gmail.com>
2023-04-15 14:14:49 -0300

    Lidando com responsividade

--- index.php
+++ index.php
@@ -1,3 +1,93 @@
+<?php
+  $products = [
+    (object)[
+      'id' => 1,
+      'name' => 'Camiseta básica',
+      'slug' => 'camiseta-basica',
+      'description' => 'Camiseta básica de algodão',
+      'price' => 29.90,
+      'image' => 'https://cdn.awsli.com.br/1000x1000/608/608801/produto/133246931/44b83527a3.jpg',
+      'additional_images' => null,
+      'size' => 'M',
+      'color' => 'Branco',
+      'stock_qtd' => 50,
+      'category_id' => 1,
+      'group_id' => null
+    ],(object)[
+      'id' => 2,
+      'name' => 'Camiseta estampada',
+      'slug' => 'camiseta-estampada',
+      'description' => 'Camiseta estampada em algodão',
+      'price' => 39.90,
+      'image' => 'https://hugobossstore.vtexassets.com/arquivos/ids/241400/4063535199447-Conjunto-3-Camisetas-BOSS-Gola-Redonda-Classic-Multicolorido_1.jpg?v=638025897554430000',
+      'additional_images' => null,
+      'size' => 'L',
+      'color' => 'Azul',
+      'stock_qtd' => 30,
+      'category_id' => 2,
+      'group_id' => null
+    ],(object)[
+      'id' => 3,
+      'name' => 'Camiseta polo',
+      'slug' => 'camiseta-polo',
+      'description' => 'Camiseta polo em algodão',
+      'price' => 59.90,
+      'image' => 'https://ae01.alicdn.com/kf/H5d9ddc1882fb4700aff0eb5ca0f5199eX/Camiseta-con-estampado-Vintage-hecho-en-1993-para-hombre-con-todas-las-partes-originales-Camisa-de.jpg',
+      'additional_images' => null,
+      'size' => 'XL',
+      'color' => 'Preto',
+      'stock_qtd' => 20,
+      'category_id' => 1,
+      'group_id' => 1
+    ],
+
+    (object)[
+      'id' => 1,
+      'name' => 'Camiseta básica',
+      'slug' => 'camiseta-basica',
+      'description' => 'Camiseta básica de algodão',
+      'price' => 29.90,
+      'image' => 'https://cdn.awsli.com.br/1000x1000/608/608801/produto/133246931/44b83527a3.jpg',
+      'additional_images' => null,
+      'size' => 'M',
+      'color' => 'Branco',
+      'stock_qtd' => 50,
+      'category_id' => 1,
+      'group_id' => null
+    ],(object)[
+      'id' => 2,
+      'name' => 'Camiseta estampada',
+      'slug' => 'camiseta-estampada',
+      'description' => 'Camiseta estampada em algodão',
+      'price' => 39.90,
+      'image' => 'https://hugobossstore.vtexassets.com/arquivos/ids/241400/4063535199447-Conjunto-3-Camisetas-BOSS-Gola-Redonda-Classic-Multicolorido_1.jpg?v=638025897554430000',
+      'additional_images' => null,
+      'size' => 'L',
+      'color' => 'Azul',
+      'stock_qtd' => 30,
+      'category_id' => 2,
+      'group_id' => null
+    ],(object)[
+      'id' => 3,
+      'name' => 'Camiseta polo',
+      'slug' => 'camiseta-polo',
+      'description' => 'Camiseta polo em algodão',
+      'price' => 59.90,
+      'image' => 'https://ae01.alicdn.com/kf/H5d9ddc1882fb4700aff0eb5ca0f5199eX/Camiseta-con-estampado-Vintage-hecho-en-1993-para-hombre-con-todas-las-partes-originales-Camisa-de.jpg',
+      'additional_images' => null,
+      'size' => 'XL',
+      'color' => 'Preto',
+      'stock_qtd' => 20,
+      'category_id' => 1,
+      'group_id' => 1
+    ],
+  ];
+
+  $products = array_map(function($product){
+    $product->price_formatted = 'R$ ' . number_format($product->price, 2, ',', '.');
+    return $product;
+  }, $products);
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -20,6 +110,45 @@
     h1, strong, svg{ color: white; }
     .text-sm{ font-size: 0.875rem; }
     .text-xs{ font-size: 0.7rem;   }
+
+    #container-products{
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      gap: .4rem;
+    }
+    .card-product{
+      width: 100%;
+      max-width: 27rem;
+      margin: auto;
+    }
+    .card-product .card-img-top{
+      height: 20rem;
+      object-fit: cover;
+      object-position: center;
+    }
+
+    /* 
+      Small	sm	≥576px
+      Medium	md	≥768px
+      Large	lg	≥992px
+      Extra large	xl	≥1200px
+      Extra extra large	xxl	≥1400px
+    */
+    @media (min-width: 576px){
+      #container-products{
+        grid-template-columns: repeat(2, 1fr);
+      }
+    }
+    @media (min-width: 992px){
+      #container-products{
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+    @media (min-width: 1400px){
+      .card-product .card-img-top{
+        height: 24rem;
+      }  
+    }    
   </style>
 </head>
 <body>
@@ -31,7 +160,7 @@
       </div>
   
       <div class="d-flex align-items-center" style="gap: 0.7rem;">
-        <div class="d-flex flex-column text-end">
+        <div class="d-none d-sm-flex flex-column text-end">
           <strong class="text-sm">Meu Carrinho</strong>
           <span class="text-xs fw-light">0 itens</span>
         </div>
@@ -39,8 +168,8 @@
       </div>
     </nav>
 
-    <section class="mt-5">
-      <?php include 'src/components/product-item.php'; ?>
+    <section class="mt-3 mt-sm-4 mt-lg-5 mb-5 px-2 px-lg-4" id="container-products">
+      <?php foreach($products as $product){ include 'src/components/product-item.php'; } ?>
     </section>
   </main>
   <script
--- src/components/product-item.php
+++ src/components/product-item.php
@@ -1,13 +1,13 @@
-<div class="card-product card" style="width: 18rem;">
+<div class="card-product card">
   <div class="card-body">
     <img
-      src="https://cdn.awsli.com.br/1000x1000/608/608801/produto/133246931/44b83527a3.jpg"
+      src="<?php echo $product->image; ?>"
       alt="Imagem da camisa software developer preta"
       class="card-img-top rounded shadow"
     />
     <div class="d-flex flex-column mt-2">
-      <h5 class="card-title fs-6 mb-0">Camisa Software Developer preta</h5>
-      <span class="fs-4 fw-semibold">R$ 179,90</span>
+      <h5 class="card-title fs-6 mb-0"><?php echo $product->name; ?></h5>
+      <span class="fs-4 fw-semibold"><?php echo $product->price_formatted; ?></span>
 
       <button type="button" class="btn btn-primary mt-3 d-flex align-items-center p-0 text-sm overflow-hidden">
         <div
