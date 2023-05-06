01948b5e339adf076e11e3ac9e3a2d6445e75f74
Mateus Brandão <mateusfleria@gmail.com>
2023-05-06 11:56:44 -0300

    Adicionando offcanvas e criando layout do carrinho em html

--- index.php
+++ index.php
@@ -40,47 +40,6 @@
       'category_id' => 1,
       'group_id' => 1
     ],
-
-    (object)[
-      'id' => 1,
-      'name' => 'Camiseta básica',
-      'slug' => 'camiseta-basica',
-      'description' => 'Camiseta básica de algodão',
-      'price' => 29.90,
-      'image' => 'https://cdn.awsli.com.br/1000x1000/608/608801/produto/133246931/44b83527a3.jpg',
-      'additional_images' => null,
-      'size' => 'M',
-      'color' => 'Branco',
-      'stock_qtd' => 50,
-      'category_id' => 1,
-      'group_id' => null
-    ],(object)[
-      'id' => 2,
-      'name' => 'Camiseta estampada',
-      'slug' => 'camiseta-estampada',
-      'description' => 'Camiseta estampada em algodão',
-      'price' => 39.90,
-      'image' => 'https://hugobossstore.vtexassets.com/arquivos/ids/241400/4063535199447-Conjunto-3-Camisetas-BOSS-Gola-Redonda-Classic-Multicolorido_1.jpg?v=638025897554430000',
-      'additional_images' => null,
-      'size' => 'L',
-      'color' => 'Azul',
-      'stock_qtd' => 30,
-      'category_id' => 2,
-      'group_id' => null
-    ],(object)[
-      'id' => 3,
-      'name' => 'Camiseta polo',
-      'slug' => 'camiseta-polo',
-      'description' => 'Camiseta polo em algodão',
-      'price' => 59.90,
-      'image' => 'https://ae01.alicdn.com/kf/H5d9ddc1882fb4700aff0eb5ca0f5199eX/Camiseta-con-estampado-Vintage-hecho-en-1993-para-hombre-con-todas-las-partes-originales-Camisa-de.jpg',
-      'additional_images' => null,
-      'size' => 'XL',
-      'color' => 'Preto',
-      'stock_qtd' => 20,
-      'category_id' => 1,
-      'group_id' => 1
-    ],
   ];
 
   $products = array_map(function($product){
@@ -164,13 +123,23 @@
           <strong class="text-sm">Meu Carrinho</strong>
           <span class="text-xs fw-light">0 itens</span>
         </div>
-        <div><?php include 'src/components/icons/cart.php'; ?></div>
+
+        <button
+          class="btn p-0 m-0"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#cartOffcanvas"
+          aria-controls="cartOffcanvas"
+        >
+          <?php include 'src/components/icons/cart.php'; ?>
+        </button>
       </div>
     </nav>
 
     <section class="mt-3 mt-sm-4 mt-lg-5 mb-5 px-2 px-lg-4" id="container-products">
       <?php foreach($products as $product){ include 'src/components/product-item.php'; } ?>
     </section>
+    <?php include 'src/components/cart-offcanvas.php'; ?>
   </main>
   <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
---
+++ src/components/cart-offcanvas.php
@@ -0,0 +1,35 @@
+<div class="offcanvas offcanvas-start" tabindex="-1" id="cartOffcanvas" aria-labelledby="cartOffcanvasLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="cartOffcanvasLabel">Carrinho</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item py-3 px-1">
+        <div class="d-flex align-items-center column-gap-3">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1505/8214/products/4_1_1500x.jpg?v=1594931500"
+            alt="Imagem da camisa software developer preta"
+            class="rounded shadow"
+            style="width: 5rem; height: 5rem; object-fit: cover;"
+          />
+          <div class="d-flex flex-column">
+            <h6 class="mb-1">Camisa Malha Dupla</h6>
+            <span class="text-muted">R$ 89,99</span>
+          </div>
+          <div class="ms-auto border rounded d-flex">
+            <button
+              type="button"
+              class="btn m-0 fw-bold text-muted"
+            >+</button>
+            <span class="align-self-center px-1 text-sm text-muted">1</span>
+            <button
+              type="button"
+              class="btn m-0 fw-bold text-muted"
+            >-</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
